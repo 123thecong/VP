085d0b56e446c20b5cbab966039d9798febbb101
123thecong <123THECONG@gmail.com>
2025-03-10 13:03:19 +0700

    Update ANYDESK.cmd

--- ANYDESK.cmd
+++ ANYDESK.cmd
@@ -1,19 +1,5 @@
 @echo off
-echo ---------- Import profile Wifi ----------
-echo.
-echo ---------- Import profile Wifi HA NAM ----------
-netsh wlan add profile filename=%~dp0VN0122.xml
---netsh wlan add profile filename=%~dp0supperwifi.xml
---netsh wlan add profile filename=%~dp0kubo.xml
---netsh wlan add profile filename=%~dp0IOT-hanam.xml
-echo.
-echo.
-echo.
-echo ---------- Import profile Wifi VINH PHUC----------
-netsh wlan add profile filename=%~dp0vnpost.xml
-netsh wlan add profile filename=%~dp0kubo.xml
---netsh wlan add profile filename=%~dp0IOT-VINHPHUC.xml
-echo ---------- Remove installation files ----------
+echo ---------- Dang cai dat Anydesk ----------
 %~dp0anydesk.exe --install  "C:\Program Files (x86)\AnyDesk" --start-with-win --create-desktop-icon
 timeout /t 30
 echo Vinhphuc@2021 | "C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --set-password
