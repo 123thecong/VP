52ec17caf78d7a96f1c9f13cbc4449710e520820
123thecong <123THECONG@gmail.com>
2025-04-01 15:20:27 +0700

    ok

--- NGW-UAT/app/src/main/java/kr/ac/konkuk/ngwuat/MainActivity.kt
+++ NGW-UAT/app/src/main/java/kr/ac/konkuk/ngwuat/MainActivity.kt
@@ -3,10 +3,10 @@ package com.congit.ngwuat
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.http.SslError
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
-import android.webkit.URLUtil
-import android.webkit.WebView
+import android.webkit.*
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
@@ -15,39 +15,15 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import java.text.SimpleDateFormat
 import java.util.*
 
-
 class MainActivity : AppCompatActivity() {
 
-    //định nghĩa thuộc tính
-    private val goHomeButton: ImageButton by lazy {
-        findViewById(R.id.btn_goHome)
-    }
-    //private val goHome5Button: ImageButton by lazy {
-    //     findViewById(R.id.btn_goHome5)
-    //  }
-    private val addressBar: EditText by lazy {
-        findViewById(R.id.addressBar)
-    }
-
-    private val goBackButton: ImageButton by lazy {
-        findViewById(R.id.btn_goBack)
-    }
-
-    private val goForwardButton: ImageButton by lazy {
-        findViewById(R.id.btn_goForward)
-    }
-
-    private val webView: WebView by lazy {
-        findViewById(R.id.webView)
-    }
-
-    private val refreshLayout: SwipeRefreshLayout by lazy {
-        findViewById(R.id.refreshLayout)
-    }
-
-    private val progressBar: ContentLoadingProgressBar by lazy {
-        findViewById(R.id.progressBar)
-    }
+    private val goHomeButton: ImageButton by lazy { findViewById(R.id.btn_goHome) }
+    private val addressBar: EditText by lazy { findViewById(R.id.addressBar) }
+    private val goBackButton: ImageButton by lazy { findViewById(R.id.btn_goBack) }
+    private val goForwardButton: ImageButton by lazy { findViewById(R.id.btn_goForward) }
+    private val webView: WebView by lazy { findViewById(R.id.webView) }
+    private val refreshLayout: SwipeRefreshLayout by lazy { findViewById(R.id.refreshLayout) }
+    private val progressBar: ContentLoadingProgressBar by lazy { findViewById(R.id.progressBar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,13 +35,8 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun initViews() {
-        // Để tải một ứng dụng không phải là trình duyệt ứng dụng mặc định, bạn phải ghi đè hành vi của chế độ xem web.
-        // Mã này được yêu cầu để mở trang web mong muốn trong khu vực xem web được chỉ định.
-
-        //webXem nhiều hơn ba lần. Vì nó truy cập nên nếu bạn liên kết nó với áp dụng, bạn không cần phải gọi chế độ xem web quá ba lần.
-        webView.apply{
+        webView.apply {
             webViewClient = WebViewClient()
-            // Cho phép thực hiện các hoạt động được triển khai bằng JavaScript trong chế độ xem web
             webChromeClient = WebChromeClient()
             settings.javaScriptEnabled = true
             loadUrl(DEFAULT_URL)
@@ -75,12 +46,10 @@ class MainActivity : AppCompatActivity() {
     fun openDeviceSettings() {
         startActivity(Intent("android.settings.SETTINGS"))
     }
+
     fun stagenow() {
         startActivity(Intent("com.symbol.tool.stagenow.main.HomeScreen"))
     }
-
-
-
 
     @SuppressLint("SimpleDateFormat")
     private fun bindViews() {
@@ -93,123 +62,73 @@ class MainActivity : AppCompatActivity() {
         val pass5 = "rf.congit.online"
         val pass6 = "stagenow"
         val pass7 = "ngw"
-        addressBar.setOnEditorActionListener { v, actionId, event ->
+
+        addressBar.setOnEditorActionListener { v, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 val loadingUrl = v.text.toString()
                 when {
                     loadingUrl.equals(pass1, ignoreCase = true) ||
                             loadingUrl.equals(pass2, ignoreCase = true) ||
-                            loadingUrl.equals(pass3, ignoreCase = true) -> {
-                        // Nếu người dùng nhập pass giống thì mở Fun settings
-                        openDeviceSettings()
-                    }
-                    loadingUrl.equals(pass4, ignoreCase = true) -> {
-                        // Nếu người dùng nhập pass giống thì tải URL Google
-                        webView.loadUrl("http://google.com")
-                    }
-                    loadingUrl.equals(pass5, ignoreCase = true) -> {
-                        // Nếu người dùng nhập pass giống thì tải URL rf.congit.online
-                        webView.loadUrl("http://rf.congit.online")
-                    }
-                    // Nếu người dùng nhập pass giống thì mở ứng dụng stagenow
-                    loadingUrl.equals(pass6, ignoreCase = true) -> {
-                        stagenow()
-                    }
-                    loadingUrl.equals(pass7, ignoreCase = true) -> {
-                        // Nếu người dùng nhập pass giống thì tải URL rf.congit.online
-                        webView.loadUrl("https://ngwsceprod.ap.signintra.com/prdo2/sce/mobile-web-client/inforMetaClient.html")
-                    }
-                    URLUtil.isNetworkUrl(loadingUrl) -> {
-                        // Nếu URL hợp lệ, tải URL vào WebView
-                        webView.loadUrl("https://rhosceappuat01.ap.signintra.com/uat01/sce/mobile-web-client/inforMetaClient.html")
-                    }
-                    else -> {
-                        // Thêm http:// vào phía trước URL không hợp lệ
-                        webView.loadUrl("https://rhosceappuat01.ap.signintra.com/uat01/sce/mobile-web-client/inforMetaClient.html")
-                    }
+                            loadingUrl.equals(pass3, ignoreCase = true) -> openDeviceSettings()
+
+                    loadingUrl.equals(pass4, ignoreCase = true) -> webView.loadUrl("http://google.com")
+                    loadingUrl.equals(pass5, ignoreCase = true) -> webView.loadUrl("http://rf.congit.online")
+                    loadingUrl.equals(pass6, ignoreCase = true) -> stagenow()
+                    loadingUrl.equals(pass7, ignoreCase = true) -> webView.loadUrl("https://ngwsceprod.ap.signintra.com/prdo2/sce/mobile-web-client/inforMetaClient.html")
+
+                    URLUtil.isNetworkUrl(loadingUrl) -> webView.loadUrl(loadingUrl)
+                    else -> webView.loadUrl(DEFAULT_URL)
                 }
             }
-
-
-
-                // Tải URL cố định vào WebView
-                //webView.loadUrl("https://rhosceappuat01.ap.signintra.com/uat01/sce/mobile-web-client/inforMetaClient.html")
-
-
-
-            // Để hạ bàn phím xuống
-            return@setOnEditorActionListener false
+            false
         }
 
-        goBackButton.setOnClickListener {
-            webView.goBack()
-        }
-
-        goForwardButton.setOnClickListener {
-            webView.goForward()
-        }
-
-        goHomeButton.setOnClickListener {
-            webView.loadUrl(DEFAULT_URL)
-        }
-        //  goHome5Button.setOnClickListener {
-        //      webView.loadUrl("http://rf.congit.click")
-        //  }
-        refreshLayout.setOnRefreshListener {
-            webView.reload()
-        }
+        goBackButton.setOnClickListener { if (webView.canGoBack()) webView.goBack() }
+        goForwardButton.setOnClickListener { if (webView.canGoForward()) webView.goForward() }
+        goHomeButton.setOnClickListener { webView.loadUrl(DEFAULT_URL) }
+        refreshLayout.setOnRefreshListener { webView.reload() }
     }
 
-    // Nếu không đính kèm bên trong, bạn sẽ không thể truy cập các thuộc tính của hoạt động chính -> Bạn sẽ không thể truy cập RefreshLayout
-    //Bằng cách thêm //inner, việc truy cập vào lớp cha sẽ trở nên khả thi.
-    // Được sử dụng để hiển thị chế độ xem web
-    inner class WebViewClient: android.webkit.WebViewClient() {
+    inner class WebViewClient : android.webkit.WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            view?.loadUrl(request?.url.toString())
+            return true
+        }
 
-        //Thực hiện hiện và biến mất
+        override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+            handler?.proceed() // Bỏ qua lỗi SSL (cẩn thận khi dùng trong môi trường production)
+        }
+
         override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
             super.onPageStarted(view, url, favicon)
             progressBar.show()
         }
 
-        //Khi tải trang hoàn tất
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
-
             refreshLayout.isRefreshing = false
             progressBar.hide()
-            //Tắt nút quay lại nếu không có cửa sổ để quay lại
             goBackButton.isEnabled = webView.canGoBack()
-            //앞으로 ''
-            goForwardButton.isEnabled = webView.canGoBack()
-
-            //Đặt địa chỉ được tải cuối cùng (m.naver.com (url cuối cùng), giống như khi bạn tìm kiếm bằng www.naver.com, kết quả được hiển thị dưới dạng m.naver.com)
+            goForwardButton.isEnabled = webView.canGoForward()
             addressBar.setText(url)
-
         }
     }
 
-    // Thường được sử dụng khi ghi đè các sự kiện (phối cảnh) cấp trình duyệt
-    inner class WebChromeClient: android.webkit.WebChromeClient() {
+    inner class WebChromeClient : android.webkit.WebChromeClient() {
         override fun onProgressChanged(view: WebView?, newProgress: Int) {
             super.onProgressChanged(view, newProgress)
-
-            progressBar.progress = newProgress //DEFAULT
+            progressBar.progress = newProgress
         }
     }
 
-
-
-
     override fun onBackPressed() {
-        //뒤로 갈 수 있는지를 확인
-        if(webView.canGoBack()){
+        if (webView.canGoBack()) {
             webView.goBack()
         } else {
             super.onBackPressed()
         }
     }
 
-//Trang mặc định
     companion object {
         private const val DEFAULT_URL = "https://rhosceappuat01.ap.signintra.com/uat01/sce/mobile-web-client/inforMetaClient.html"
     }
